0952c17e22e1a24e38540a65aeae8ca8d0ad2bdd
MForever78 <shunjian1128@gmail.com>
2016-01-04 18:27:33 +0800

    Complete experiment 6

--- A6/src/main.c
+++ A6/src/main.c
@@ -5,19 +5,28 @@
 #include <GLUT/glut.h>
 
 #define BITMAP_ID 0x4D42
+#define CHECKHEIGHT 64
+#define CHECKWIDTH 64
 
 float fTranslate;
 float fRotate;
-float fScale     = 1.0f;	// set inital scale value to 1.0f
+float fScale = 1.0f;	// set inital scale value to 1.0f
 
 bool bPersp = false;
 bool bAnim = false;
 bool bWire = false;
+bool bLight = false;
+bool bMultiTexture = false;
 
 int wHeight = 0;
 int wWidth = 0;
 
-GLuint texture[3];
+// 0: Monet
+// 1: Crack
+// 2: Spot
+// 3: Check
+GLuint texture[4];
+GLubyte checkImage[CHECKHEIGHT][CHECKWIDTH][3];
 
 void Draw_Leg();
 
@@ -81,7 +90,8 @@ unsigned char *loadBitmapFile(char *filename, BITMAPINFOHEADER *bitmapInfoHeader
     fread(&bitmapInfoHeader->biClrUsed, 4, 1, filePtr);
     fread(&bitmapInfoHeader->biClrImportant, 4, 1, filePtr);
 
-    // print out the info header
+    // print out the info header for debug
+    /*
     printf("biSize: %d\n", bitmapInfoHeader->biSize);
     printf("biWidth: %d\n", bitmapInfoHeader->biWidth);
     printf("biHeight: %d\n", bitmapInfoHeader->biHeight);
@@ -89,6 +99,7 @@ unsigned char *loadBitmapFile(char *filename, BITMAPINFOHEADER *bitmapInfoHeader
     printf("biBitCount: %d\n", bitmapInfoHeader->biBitCount);
     printf("biCompression: %hu\n", bitmapInfoHeader->biBitCount);
     printf("biSizeImage: %u\n", bitmapInfoHeader->biSizeImage);
+    */
 
     // move pointer to the begining of bitmap data
     fseek(filePtr, bitmapFileHeader.bfOffBytes, SEEK_SET);
@@ -126,7 +137,7 @@ unsigned char *loadBitmapFile(char *filename, BITMAPINFOHEADER *bitmapInfoHeader
     return bitmapImage;
 }
 
-void loadTexture(int i, char *filename) {
+void loadTexture(int i, char *filename, bool light) {
     BITMAPINFOHEADER bitmapInfoHeader;
     unsigned char *bitmapData;
 
@@ -145,43 +156,246 @@ void loadTexture(int i, char *filename) {
             GL_RGB,                     // bitmap color mode
             GL_UNSIGNED_BYTE,           // bitmap data type
             bitmapData);                // bitmap data pointer
+    if (light)
+        glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_COMBINE);
+}
+
+void makeCheckImage() {
+    int i, j, c;
+
+    for (i = 0; i < CHECKHEIGHT; i++) {
+        for (j = 0; j < CHECKWIDTH; j++) {
+            c = (((i & 0x8) == 0) ^ ((j & 0x8) == 0)) * 255;
+            checkImage[i][j][0] = (GLubyte) c;
+            checkImage[i][j][1] = (GLubyte) 0;
+            checkImage[i][j][2] = (GLubyte) 0;
+        }
+    }
 }
 
 void initTexture() {
-    glGenTextures(3, texture);
-    loadTexture(0, "Monet.bmp");
-    loadTexture(1, "Crack.bmp");
+    glGenTextures(4, texture);
+    loadTexture(0, "Monet.bmp", true);
+    loadTexture(1, "Crack.bmp", false);
+    loadTexture(2, "Spot.bmp", false);
 
-    // TODO: generate custom texture
+    makeCheckImage();
 
-    /*
-    glBindTexture(GL_TEXTURE_2D, texture[2]);
-    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-    glTexImage2D(GL_TEXTURE_2D, 0, 3, TEXW, TEXH, 0, GL_RGB, GL_UNSIGNED_BYTE, tex);
+    glBindTexture(GL_TEXTURE_2D, texture[3]);
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, CHECKWIDTH, CHECKHEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, checkImage);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    */
+}
+
+void drawQuadsWithTexture() {
+    glPushMatrix();
+    glBegin(GL_QUADS);
+        glTexCoord2i(1, 1);
+        glVertex3i(1, 1, 0);
+        
+        glTexCoord2i(1, 0);
+        glVertex3i(1, -1, 0);
+
+        glTexCoord2i(0, 0);
+        glVertex3i(-1, -1, 0);
+        
+        glTexCoord2i(0, 1);
+        glVertex3i(-1, 1, 0);
+    glEnd();
+    glPopMatrix();
+}
+
+void cubeWithTexture(GLfloat length, GLuint texture) {
+    GLfloat halfLength = length / 2;
+
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glPushMatrix();
+    glScalef(halfLength, halfLength, halfLength);
+
+    // front
+    glPushMatrix();
+    glTranslatef(0, 0, 1);
+    drawQuadsWithTexture();
+    glPopMatrix();
+
+    // back
+    glPushMatrix();
+    glTranslatef(0, 0, -1);
+    drawQuadsWithTexture();
+    glPopMatrix();
+
+    // left
+    glPushMatrix();
+    glRotatef(-90, 0, 1, 0);
+    glTranslatef(0, 0, 1);
+    drawQuadsWithTexture();
+    glPopMatrix();
+
+    // right
+    glPushMatrix();
+    glRotatef(90, 0, 1, 0);
+    glTranslatef(0, 0, 1);
+    drawQuadsWithTexture();
+    glPopMatrix();
+
+    // upper
+    glPushMatrix();
+    glRotatef(90, 1, 0, 0);
+    glTranslatef(0, 0, 1);
+    drawQuadsWithTexture();
+    glPopMatrix();
+
+    // bottom
+    glPushMatrix();
+    glRotatef(-90, 1, 0, 0);
+    glTranslatef(0, 0, 1);
+    drawQuadsWithTexture();
+    glPopMatrix();
+
+    glPopMatrix();
+    glDisable(GL_TEXTURE_2D);
+}
+
+void cubeWithMultiTexture(GLfloat length, GLuint texture0, GLuint texture1) {
+    GLfloat halfLength = length / 2;
+
+    glPushMatrix();
+    glScalef(halfLength, halfLength, halfLength);
+
+    // front
+    glPushMatrix();
+    glTranslatef(0, 0, 1);
+    // only use multitexture on the front
+    glActiveTextureARB(GL_TEXTURE0_ARB);
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, texture0);
+    glActiveTextureARB(GL_TEXTURE1_ARB);
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    // mixture textures
+    glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_COMBINE_ARB);
+    // compute method
+    glTexEnvi(GL_TEXTURE_ENV, GL_COMBINE_RGB_ARB, GL_INTERPOLATE_ARB);
+    // determine argvs
+    
+    // Previous: texture 0
+    glTexEnvi(GL_TEXTURE_ENV, GL_SOURCE0_RGB_ARB, GL_PREVIOUS_ARB);
+    glTexEnvi(GL_TEXTURE_ENV, GL_OPERAND0_RGB_ARB, GL_SRC_COLOR);
+
+    // Current: texture 1
+    glTexEnvi(GL_TEXTURE_ENV, GL_SOURCE1_RGB_ARB, GL_TEXTURE);
+    glTexEnvi(GL_TEXTURE_ENV, GL_OPERAND1_RGB_ARB, GL_SRC_COLOR);
+
+    // Environment
+    glTexEnvi(GL_TEXTURE_ENV, GL_SOURCE2_RGB_ARB, GL_PRIMARY_COLOR_ARB);
+    glTexEnvi(GL_TEXTURE_ENV, GL_OPERAND2_RGB_ARB, GL_SRC_COLOR);
+
+    // Draw
+    glBegin(GL_QUADS);
+        glMultiTexCoord2fARB(GL_TEXTURE0_ARB, 1, 1);
+        glMultiTexCoord2fARB(GL_TEXTURE1_ARB, 1, 1);
+        glVertex3i(1, 1, 0);
+        
+        glMultiTexCoord2fARB(GL_TEXTURE0_ARB, 1, 0);
+        glMultiTexCoord2fARB(GL_TEXTURE1_ARB, 1, 0);
+        glVertex3i(1, -1, 0);
+
+        glMultiTexCoord2fARB(GL_TEXTURE0_ARB, 0, 0);
+        glMultiTexCoord2fARB(GL_TEXTURE1_ARB, 0, 0);
+        glVertex3i(-1, -1, 0);
+        
+        glMultiTexCoord2fARB(GL_TEXTURE0_ARB, 0, 1);
+        glMultiTexCoord2fARB(GL_TEXTURE1_ARB, 0, 1);
+        glVertex3i(-1, 1, 0);
+    glEnd();
+
+    glActiveTextureARB(GL_TEXTURE1_ARB);
+    glDisable(GL_TEXTURE_2D);
+    glActiveTextureARB(GL_TEXTURE0_ARB);
+    glEnable(GL_TEXTURE_2D);
+    glDisable(GL_TEXTURE_2D);
+    glPopMatrix();
+
+    // use single texture
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, texture0);
+
+    // back
+    glPushMatrix();
+    glTranslatef(0, 0, -1);
+    drawQuadsWithTexture();
+    glPopMatrix();
+
+    // left
+    glPushMatrix();
+    glRotatef(-90, 0, 1, 0);
+    glTranslatef(0, 0, 1);
+    drawQuadsWithTexture();
+    glPopMatrix();
+
+    // right
+    glPushMatrix();
+    glRotatef(90, 0, 1, 0);
+    glTranslatef(0, 0, 1);
+    drawQuadsWithTexture();
+    glPopMatrix();
+
+    // upper
+    glPushMatrix();
+    glRotatef(90, 1, 0, 0);
+    glTranslatef(0, 0, 1);
+    drawQuadsWithTexture();
+    glPopMatrix();
+
+    // bottom
+    glPushMatrix();
+    glRotatef(-90, 1, 0, 0);
+    glTranslatef(0, 0, 1);
+    drawQuadsWithTexture();
+    glPopMatrix();
+
+    glDisable(GL_TEXTURE_2D);
+    glPopMatrix();
+}
+
+void drawTable() {
+    if (bMultiTexture)
+        cubeWithMultiTexture(1.0, texture[1], texture[2]);
+    else
+        cubeWithTexture(1.0, texture[1]);
 }
 
 void Draw_Scene()
 {
-    glEnable(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, texture[0]);
+    GLfloat teapot_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+    glMaterialfv(GL_FRONT, GL_SPECULAR, teapot_specular);
+    glMateriali(GL_FRONT_AND_BACK, GL_SHININESS, 60);
 
 	glPushMatrix();
+
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, texture[3]);
+
 	glTranslatef(0, 0, 4+1);
 	glRotatef(90, 1, 0, 0);
 	glutSolidTeapot(1);
-	glPopMatrix();
 
     glDisable(GL_TEXTURE_2D);
+	glPopMatrix();
+
+
+    GLfloat table_specular[] = { 0, 0, 0, 0 };
+    glMaterialfv(GL_FRONT, GL_SPECULAR, table_specular);
+    glMateriali(GL_FRONT_AND_BACK,GL_SHININESS,0);
 
 	glPushMatrix();
 	glTranslatef(0, 0, 3.5);
 	glScalef(5, 4, 1);
-	glutSolidCube(1.0);
+    drawTable();
 	glPopMatrix();
 
 	glPushMatrix();
@@ -209,7 +423,7 @@ void Draw_Scene()
 void Draw_Leg()
 {
 	glScalef(1, 1, 3);
-	glutSolidCube(1.0);
+	cubeWithTexture(1.0, texture[1]);
 }
 
 void updateView(int width, int height)
@@ -261,6 +475,8 @@ void key(unsigned char k, int x, int y)
 
 	case ' ': {bAnim = !bAnim; break;}
 	case 'o': {bWire = !bWire; break;}
+    case 'l': {bLight = !bLight; break;}
+    case 't': {bMultiTexture = !bMultiTexture; break;}
 
 	case 'a': {
 		eye[0] -= 0.2f;
@@ -317,11 +533,16 @@ void redraw()
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_LIGHTING);
 	GLfloat white[] = { 1.0, 1.0, 1.0, 1.0 };
-	GLfloat light_pos[] = {5,5,5,1};
+	GLfloat light_pos[] = {0,0,5,1};
 
 	glLightfv(GL_LIGHT0, GL_POSITION, light_pos);
 	glLightfv(GL_LIGHT0, GL_AMBIENT, white);
-	glEnable(GL_LIGHT0);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, white);
+
+    if (bLight)
+        glEnable(GL_LIGHT0);
+    else
+        glDisable(GL_LIGHT0);
 
 	glRotatef(fRotate, 0, 1.0f, 0);			// Rotate around Y axis
 	glRotatef(-90, 1, 0, 0);
